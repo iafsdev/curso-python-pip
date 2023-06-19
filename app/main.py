18903ea93fdb5f9437f2120b9d41dbb01d6da41e
iafs32 <iafs.0232@gmail.com>
2023-06-19 17:44:52 -0600

    update de app project for save the charts apart with their countrries names

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ import read_csv
 import charts
 
 def run():
-  data = read_csv.read_csv('./app/data.csv')
+  data = read_csv.read_csv('data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America', data))
 
-  '''
+  countries = list(map(lambda x : x['Country/Territory'], data))
+  percentages = list(map(lambda x : x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
   country = input('Type country: ')
   
   result = utils.population_by_country(data, country)
@@ -13,11 +17,7 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-  '''
-
-  labels, values = utils.get_world_population_percentage(data)
-  charts.generate_pie_chart(labels, values)
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
 
 if __name__ == '__main__':
   run()
